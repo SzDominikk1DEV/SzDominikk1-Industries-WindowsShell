dd4912e55aff5a4271c18ea9ab6a4b6744ca0f3f
SzDominikk1DEV <szuromidominikdesign@gmail.com>
2025-08-21 22:39:52 +0200

    Hozzáadva a WindowsShell Visual Studio projekt

---
+++ WindowsShell/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WindowsShell
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InstallChocolatey_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = "Set-ExecutionPolicy Bypass -Scope Process -Force; " +
+                                "[System.Net.ServicePointManager]::SecurityProtocol = " +
+                                "[System.Net.ServicePointManager]::SecurityProtocol -bor 3072; " +
+                                "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))",
+                    Verb = "runas", // futtatás admin módban
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hiba történt: {ex.Message}");
+            }
+        }
+
+        private void RebootUEFI_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "shutdown.exe",
+                    Arguments = "/r /fw /t 0",
+                    Verb = "runas",
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hiba történt: {ex.Message}");
+            }
+        }
+    }
+}
